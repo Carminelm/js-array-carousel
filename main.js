0d7d3aaa3dd44cbd4cf41be549a23a6dfca8665f
Carminelm <carminelm54@gmail.com>
2024-06-10 15:51:24 +0200

    created slide content

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ const imgArray = ["img/01.webp",
  "img/03.webp",
  "img/04.webp",
  "img/05.webp"];
+
+ //identifichiamo il contenitore
+
+ const slide = document.querySelector(".slide");
+
+
+ // creiamo il ciclo per le img 
+
+ for (let i=0; i<imgArray.length; i++) {
+
+    ImgItem = imgArray[i]
+
+    //Inseriamo l'elemento nel contenitore 
+
+    slide.innerHTML += `<img src="${imgArray[i]}" class="hidden carouselImg"></img>`;
+    console.log ("slide");
+    
+}
