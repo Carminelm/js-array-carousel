2d6e1a58bf652c27ce0044cce7b091e794cb6ad2
Carminelm <carminelm54@gmail.com>
2024-06-10 16:53:52 +0200

    add botton

--- main.js
+++ main.js
@@ -22,7 +22,65 @@ const imgArray = ["img/01.webp",
 
     //Inseriamo l'elemento nel contenitore 
 
-    slide.innerHTML += `<img src="${imgArray[i]}" class="hidden carouselImg"></img>`;
+    slide.innerHTML += `<img src="${imgArray[i]}" class="hidden caroselloImg"></img>`;
     console.log ("slide");
     
 }
+
+//selezioniamo tutti gli elementi img in pagima 
+
+const allImgs = document.querySelectorAll(".caroselloImg");
+allImgs[0].classList.toggle("hidden");
+
+
+
+// selezioniamo i bottoni
+const indietroBtn = document.querySelector("#indietro");
+const avantiBtn = document.querySelector("#avanti");
+let index = 0;
+//indietroBtn.style.display = "none";
+
+
+//gestiamo il click su indietro
+indietroBtn.addEventListener("click", function(){
+
+    if (index >0) {
+
+        allImgs[Index].classList.toggle("hidden");
+        index--;
+        allImgs[index].classList.toggle("hidden");
+
+    }
+
+    if (index == 0) {
+
+        indietroBtn.style.display = "none";
+
+    }
+
+    avantiBtn.style.display= "block";
+
+});
+
+//gestiamo il click su avanti 
+
+avantiBtn.addEventListener("click", function(){
+
+    if (index < ImgItem.length -1) {
+
+        allImgs[index].classList.toggle("hidden");
+        index++;
+        allImgs[index].classList.toggle("hidden");
+
+    }
+
+    
+    if (index == (ImgItem.length -1)) {
+
+        nextBtn.style.display = "none";
+
+    }
+    
+    indietroBtn.style.display= "block";
+
+});
